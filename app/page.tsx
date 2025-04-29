ffbf5d6fb2d4688bba2120cf5c3f390847ec97b1
carlo castellano <carlocastellano@iMac-di-carlo.local>
2025-04-29 11:24:29 +0200

    Aggiunti file progetto RoomLikeHeaven

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Benvenuto su Room Like Heaven</h1>
+      <p>Trova la tua casa perfetta e vivi un'esperienza unica!</p>
+    </div>
+  )
+}
+
